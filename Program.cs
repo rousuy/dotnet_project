9451a8919c70350a39c3fe35d5361bf291432125
Rodrigo Usuy <rousuy@gmail.com>
2022-01-27 18:37:07 -0300

    Created abstract class and  characters

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+using projeto_dotnet.src.Entities;
+
+
+Knight arus = new Knight("Arus", 23, "Knight");
+WhiteWizard jennica = new WhiteWizard("Jennica", 23, "white wizard");
+BlackWizard topapa = new BlackWizard("Topapa", 42, "Black Wizard");
+Ninja wedge = new Ninja("Wedge", 43, "Ninja");
+
+WriteLine($@"{arus.Attack()}
+{jennica.Attack(1)}
+{jennica.Attack(7)}
+{topapa.Attack()}
+{wedge.Attack()}");
+
---
+++ src/Entities/Ninja.cs
@@ -0,0 +1,16 @@
+namespace projeto_dotnet.src.Entities
+{
+    public class Ninja : Hero
+    {   
+         public Ninja(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack(){
+            return $"{this.Name} Attacked with hes Katana.";
+        }
+    }
+}
